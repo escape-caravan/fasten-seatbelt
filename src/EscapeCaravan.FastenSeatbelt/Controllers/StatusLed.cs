4a558971c2f4859023f2aa780a8d32287cab51c7
Eduard <e.keilholz@4dotnet.nl>
2022-04-29 14:23:12 +0200

    Adding log messages

--- src/EscapeCaravan.FastenSeatbelt/Controllers/StatusLed.cs
+++ src/EscapeCaravan.FastenSeatbelt/Controllers/StatusLed.cs
@@ -8,6 +8,7 @@ namespace EscapeCaravan.FastenSeatbelt.Controllers;
 [ApiController]
 public class StatusLed : ControllerBase
 {
+    private readonly ILogger _logger;
 
     private const int Pin = 17; // GPIO 17 (Physical pin 11)
 
@@ -15,12 +16,14 @@ public class StatusLed : ControllerBase
     [HttpGet]
     public IActionResult GetPinStatus()
     {
-        var controller = new GpioController();
+        _logger.LogInformation("Request status of pin {pin}", Pin);
 
+        var controller = new GpioController();
         controller.OpenPin(Pin, PinMode.Input);
         var pinStatus = controller.Read(Pin);
         controller.ClosePin(Pin);
 
+        _logger.LogInformation("Pin status found for pin {pin}, was {status}", Pin, pinStatus);
         return Ok(new PinStatus
         {
             Pin = Pin,
@@ -32,19 +35,23 @@ public class StatusLed : ControllerBase
     [HttpPost]
     public IActionResult SetPinStatus(PinStatus dto)
     {
+        _logger.LogInformation("Request to set pin {pin}, to status {status}", Pin, dto.Status);
+
         if (dto.Pin != Pin)
         {
+            _logger.LogInformation("Uncontrollable pin addressed. Returning bad request");
             return BadRequest("This pin cannot be controlled");
         }
 
         var desiredPinValue = dto.Status.ToLower().EndsWith("high") ? 
             PinValue.High : PinValue.Low;
+        _logger.LogInformation("Desired pin status is now set to {desiredStatus}", desiredPinValue);
 
         var controller = new GpioController();
-
         controller.OpenPin(Pin, PinMode.Output);
         controller.Write(Pin, desiredPinValue);
         controller.ClosePin(Pin);
+        _logger.LogInformation("Setting new pin status succeeded");
 
         return Ok(new PinStatus()
         {
@@ -53,5 +60,10 @@ public class StatusLed : ControllerBase
         });
     }
 
+
+    public StatusLed(ILogger logger)
+    {
+        _logger = logger;
+    }
 
 }
