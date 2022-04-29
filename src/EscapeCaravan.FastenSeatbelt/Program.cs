a7ff4f0d1dad9dec87045ddfae70970332f96b98
Eduard <e.keilholz@4dotnet.nl>
2022-04-29 13:32:53 +0200

    Runable for raspberry Pi

--- src/EscapeCaravan.FastenSeatbelt/Program.cs
+++ src/EscapeCaravan.FastenSeatbelt/Program.cs
@@ -1,9 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,8 +12,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
